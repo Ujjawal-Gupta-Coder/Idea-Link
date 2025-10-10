5ba26ff6a087ee4b9b52544665af65ec72d4a669
Ujjawal Gupta <ujjawalg047@gmail.com>
2025-10-10 16:07:15 +0530

    Added: dark/light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 import { auth } from "@/auth"
-import Card from "@/components/Card"
 
 const page = async () => {
   const session = await auth();
   console.log("the session is ", session)
 
   return (
-    <div className="flex items-center justify-center font-bold text-4xl h-home-page w-full">
-      {
-        session?.user ? 
-        <Card session={session}/> 
-        : 
-        <p>You Are not Logged in !!</p> 
-      }
+    <div>
+      Implemented Light/Dark Mode
       
       
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,21 @@
 import { auth } from "@/auth";
 import SignIn from "./SignIn"
 import SignOut  from "./SignOut"
+import Image from "next/image";
+import Toggle from "./ToggleButton"
 
 const Navbar = async () => {
    const session = await auth();
   return (
-    <header>
-        <nav className="flex justify-between px-12 py-2 bg-gray-200 h-navbar">
-            <span className="font-bold text-2xl text-black">Idea Link</span>
-
-
-              {
-                session?.user ? 
-                  <ul className="flex gap-3.5 items-center">
-                    <li className="border-1 cursor-pointer px-2 py-1 rounded-2xl bg-amber-200 font-semibold text-black"> Create Post </li>
-                    <li className="border-1 cursor-pointer px-2 py-1 rounded-2xl bg-sky-200 font-semibold text-black"> {session.user.name} </li> 
-                    <SignOut />
-                  </ul>
-                :
-                  <SignIn />
-              }
-        </nav>
+    <header className="flex gap-4">
+      
+      <button className="text-primary text-4xl font-work-sans">Idea Link</button>
+      {
+        session?.user ? <SignOut /> : <SignIn />
+      }
+      
+      <Toggle />
+      <button>Switch Mode</button>
     </header>
   )
 }
