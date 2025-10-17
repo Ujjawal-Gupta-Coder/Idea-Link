c27f0a04926e1062f6f895d6800345583b4291ca
Ujjawal Gupta <ujjawalg047@gmail.com>
2025-10-17 19:26:21 +0530

    Setup: senity initialized successfully

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -10,7 +10,7 @@ export default function SignIn() {
         await signIn("google", { redirectTo: "/" })
       }}
     >
-      <Button type="submit" className="rounded-xl h-12 w-24 text-lg text-black hover:bg-primary bg-secondary cursor-pointer" > 
+      <Button type="submit" className="rounded-xl h-12 w-24 text-lg bg-primary hover:bg-secondary text-white hover:text-black cursor-pointer" > 
         Sign In
       </Button>
     </form>
